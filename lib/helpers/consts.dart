ac5577ad96bc462b3d59489d2e5017c02735f84c
rodaina <rodainaomaer@gmail.com>
2024-07-16 14:15:43 +0200

    minor changes

--- lib/helpers/consts.dart
+++ lib/helpers/consts.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String baseUrl = 'http://192.168.0.109:3232';
+String baseUrl = 'http://192.168.3.13:3232';
 Color primaryColor = const Color(0xffFFD84C);
 Color secondaryColor = const Color(0xff807A66);
 Color highlightColor = const Color(0xff4D5DFF);
